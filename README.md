adebf98e874ad7be493aa3c2e87a83e90d56caba
No-needto-recall <111343528+No-needto-recall@users.noreply.github.com>
2023-06-13 14:18:05 +0800

    初始commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# bosi
+
+Developed with Unreal Engine 4
---
+++ Source/bosi/bosiGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "bosiGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BOSI_API AbosiGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
